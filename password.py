60764797fd6deb24589823ce3e189eb034574ae3
lorderonnie <rontheking45@gmail.com>
2019-11-08 15:29:50 +0300

    added the classes on password test and password. Added also import password.

---
+++ password.py
@@ -0,0 +1,8 @@
+class Password:
+    password_list = []
+    def __init__(self,full_name,user_name,email,password_number,password_confirm):
+        self.full_name = full_name
+        self.user_name = user_name 
+        self.email = email
+        self.password_number = password_number
+        self.password_confirm = password_confirm
--- password_test.py
+++ password_test.py
@@ -1,4 +1,14 @@
 import unittest
 from password import Password
 
-class Testpassword(unittest.Testcase):
+class Testpassword(unittest.TestCase):
+    def setUp(self):
+        self.new_password = Password("Richard Ronald","Lorderonnie","rontheking45@gmail.com","421145","421145")
+    
+    def test_init(self):
+        self.assertEqual(self.new_password.full_name,"Richard Ronald")
+        self.assertEqual(self.new_password.user_name,"Lorderonnie")
+        self.assertEqual(self.new_password.email,"rontheking45@gmail.com")
+        self.assertEqual(self.new_password.password_number,"421145")
+        self.assertEqual(self.new_password.password_confirm,"421145")   
+    
