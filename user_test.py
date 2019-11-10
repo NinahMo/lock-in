1dd7fc6e7ecab0d4851566b2c5f78d5a57a9fe94
lorderonnie <rontheking45@gmail.com>
2019-11-10 19:28:53 +0300

    added the login function to my user.py

---
+++ user_test.py
@@ -0,0 +1,17 @@
+import unittest
+from user import User
+
+class Testuser(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User("Richard Ronald","KING","rontheking44@gmail.com","164534")
+    
+    def test_init(self):
+        self.assertEqual(self.new_user.full_name,"Richard Ronald")
+        self.assertEqual(self.new_user.user_name,"KING")
+        self.assertEqual(self.new_user.email,"rontheking44@gmail.com")
+        self.assertEqual(self.new_user.password_value,"164534")
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
